ce6347c9c7f96b71613be9e845def6299574f084
LeagueRaINi <razor13372@gmail.com>
2024-12-08 20:08:48 +0100

    feat: upload

---
+++ src/main.rs
@@ -0,0 +1,168 @@
+use std::io::{BufRead, BufReader, ErrorKind, Read};
+use std::path::PathBuf;
+use std::str::FromStr;
+use std::{env, fs};
+
+use clap::Parser;
+use discord_rich_presence::activity::{Activity, ActivityBuilder, AssetsBuilder, Timestamps};
+use discord_rich_presence::{activity, DiscordIpcClient};
+use models::{CharacterClass, ClassAscendency, Translations};
+use regex::{Captures, Regex};
+
+mod models;
+
+#[derive(Parser, Debug)]
+#[clap(about, author, version)]
+struct Opt {
+    /// Path to the game directory
+    #[arg(long)]
+    game_dir: PathBuf,
+
+    /// Path to translations.json
+    #[arg(long)]
+    translations: PathBuf,
+}
+
+#[derive(Debug)]
+struct ClassInfo {
+    class: CharacterClass,
+    ascendency: Option<ClassAscendency>,
+    username: String,
+    level: u16,
+}
+
+#[derive(Debug)]
+struct MapChangeInfo {
+    level: u16,
+    name: String,
+    seed: u64,
+    ts: i64,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env::set_var("RUST_LOG", env::var("RUST_LOG").unwrap_or_else(|_| "debug".into()));
+    pretty_env_logger::init();
+
+    let Opt { game_dir, translations } = Opt::parse();
+
+    let translations = fs::read_to_string(translations)?;
+    let translations: Translations = serde_json::from_str(&translations)?;
+    log::debug!("{:#?}", translations);
+
+    let log_file = game_dir.join("logs/Client.txt");
+    let log_file = fs::File::open(log_file)?;
+
+    let mut rpc = DiscordIpcClient::new("550890770056347648");
+    rpc.connect()?;
+    rpc.set_activity(ActivityBuilder::default().details("Booting up...").build())?;
+
+    let mut log_bufr = BufReader::new(log_file);
+
+    let level_up_rgx = Regex::new(r#": (\w+) \((\w+)\) is now level (\d+)"#)?;
+    let instance_rgx = Regex::new(r#"] Generating level (\d+) area "([^"]+)" with seed (\d+)"#)?;
+
+    let mut last_instance: Option<MapChangeInfo> = None;
+    let mut last_class: Option<ClassInfo> = None;
+
+    loop {
+        let mut log = String::new();
+
+        if log_bufr.read_to_string(&mut log)? == 0 {
+            // hush little cpu don't you cry
+            std::thread::sleep(std::time::Duration::from_millis(500));
+            continue;
+        }
+
+        // this is written like it is because we have to assume the character from the past logs,
+        // we dont have an api to call yet
+        for line in log.lines() {
+            if let Some(caps) = level_up_rgx.captures(line) {
+                let username = caps.get(1).map_or("", |m| m.as_str());
+                let class = caps.get(2).map_or("", |m| m.as_str());
+                let level = caps.get(3).map_or(0, |m| m.as_str().parse::<u16>().unwrap());
+                log::debug!(
+                    "Parsed level up: {{ class: {class}, name: {username}, level: {level} }}"
+                );
+
+                let ascd_class = ClassAscendency::from_str(class).ok();
+                let main_class = ascd_class.clone().map_or_else(
+                    || CharacterClass::from_str(class).unwrap(),
+                    |ascd| ascd.get_class(),
+                );
+
+                last_class = Some(ClassInfo {
+                    class: main_class,
+                    ascendency: ascd_class,
+                    username: username.to_owned(),
+                    level,
+                });
+            } else if let Some(caps) = instance_rgx.captures(line) {
+                let level = caps.get(1).map_or(0, |m| m.as_str().parse::<u16>().unwrap());
+                let name = caps.get(2).map_or("", |m| m.as_str());
+                let seed = caps.get(3).map_or(0, |m| m.as_str().parse::<u64>().unwrap());
+                log::debug!("Parsed instance: {{ lvl: {level}, name: {name}, seed: {seed} }}");
+
+                let name = translations.get_area_display_name(name).unwrap_or(name);
+                let ts = chrono::Utc::now().timestamp();
+
+                last_instance = Some(MapChangeInfo {
+                    level,
+                    name: name.to_owned(),
+                    seed,
+                    ts,
+                });
+            }
+        }
+
+        if last_class.is_some() || last_instance.is_some() {
+            let mut act_builder = ActivityBuilder::default();
+
+            if let Some(class_info) = last_class.take() {
+                act_builder = act_builder.details(&class_info.username);
+
+                let mut asset_builder = AssetsBuilder::default();
+
+                if let Some(ascd) = class_info.ascendency {
+                    asset_builder = asset_builder
+                        .large_image(ascd.get_discord_image_name())
+                        .large_text(format!("{ascd} ({})", class_info.level))
+                        .small_image(class_info.class.get_discord_image_name())
+                        .small_text(class_info.class);
+                } else {
+                    asset_builder = asset_builder
+                        .large_image(class_info.class.get_discord_image_name())
+                        .large_text(format!("{} ({})", class_info.class, class_info.level));
+                }
+
+                act_builder = act_builder.assets(asset_builder.build());
+            }
+
+            if let Some(instance_info) = last_instance.take() {
+                act_builder = act_builder
+                    .state(format!("{} ({})", &instance_info.name, instance_info.level))
+                    .timestamps(Timestamps::new(Some(instance_info.ts), None));
+            }
+
+            let act = act_builder.build();
+
+            match rpc.set_activity(act.clone()) {
+                Ok(_) => log::debug!("Updated activity"),
+                Err(e) => {
+                    if let Some(e) = e.downcast_ref::<std::io::Error>() {
+                        match e.kind() {
+                            ErrorKind::BrokenPipe => {
+                                log::error!("Connection to discord lost, attempting to reconnect");
+                                rpc.reconnect()?;
+
+                                // not entirely sure how we should handle this... but it happens quite often and i'm not sure why
+                                rpc.set_activity(act)?;
+                                log::info!("Updated activity after reconnecting");
+                            },
+                            _ => log::error!("Error updating discord presence: {}", e),
+                        }
+                    }
+                },
+            }
+        }
+    }
+}
---
+++ src/models.rs
@@ -0,0 +1,161 @@
+use std::collections::HashMap;
+use std::fmt::{self, Display, Formatter};
+use std::str::FromStr;
+
+use serde::Deserialize;
+
+#[derive(Debug, Clone)]
+pub enum CharacterClass {
+    Mercenary,
+    Monk,
+    Ranger,
+    Sorceress,
+    Warrior,
+    Witch,
+}
+
+impl FromStr for CharacterClass {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "mercenary" => Ok(Self::Mercenary),
+            "monk" => Ok(Self::Monk),
+            "ranger" => Ok(Self::Ranger),
+            "sorceress" => Ok(Self::Sorceress),
+            "warrior" => Ok(Self::Warrior),
+            "witch" => Ok(Self::Witch),
+            _ => Err(()),
+        }
+    }
+}
+
+impl fmt::Display for CharacterClass {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl CharacterClass {
+    pub fn get_ascendencies(&self) -> Option<[ClassAscendency; 2]> {
+        match self {
+            Self::Mercenary => {
+                Some([ClassAscendency::Witchhunter, ClassAscendency::GemlingLegionnaire])
+            },
+            Self::Monk => Some([ClassAscendency::AcolyteOfChayula, ClassAscendency::Invoker]),
+            Self::Ranger => Some([ClassAscendency::Deadeye, ClassAscendency::Pathfinder]),
+            Self::Sorceress => Some([ClassAscendency::Chronomancer, ClassAscendency::Stormweaver]),
+            Self::Warrior => Some([ClassAscendency::Titan, ClassAscendency::Warbringer]),
+            Self::Witch => Some([ClassAscendency::BloodMage, ClassAscendency::Infernalist]),
+        }
+    }
+
+    pub fn get_discord_image_name(&self) -> &'static str {
+        match self {
+            Self::Mercenary => "mercenary",
+            Self::Monk => "monk",
+            Self::Ranger => "ranger",
+            Self::Sorceress => "sorceress",
+            Self::Warrior => "warrior",
+            Self::Witch => "witch",
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum ClassAscendency {
+    Witchhunter,
+    GemlingLegionnaire,
+    AcolyteOfChayula,
+    Invoker,
+    Deadeye,
+    Pathfinder,
+    Chronomancer,
+    Stormweaver,
+    Titan,
+    Warbringer,
+    BloodMage,
+    Infernalist,
+}
+
+impl FromStr for ClassAscendency {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "witchhunter" => Ok(Self::Witchhunter),
+            "gemling legionnaire" => Ok(Self::GemlingLegionnaire),
+            "acolyte of chayula" => Ok(Self::AcolyteOfChayula),
+            "invoker" => Ok(Self::Invoker),
+            "deadeye" => Ok(Self::Deadeye),
+            "pathfinder" => Ok(Self::Pathfinder),
+            "chronomancer" => Ok(Self::Chronomancer),
+            "stormweaver" => Ok(Self::Stormweaver),
+            "titan" => Ok(Self::Titan),
+            "warbringer" => Ok(Self::Warbringer),
+            "blood mage" => Ok(Self::BloodMage),
+            "infernalist" => Ok(Self::Infernalist),
+            _ => Err(()),
+        }
+    }
+}
+
+impl Display for ClassAscendency {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Witchhunter => write!(f, "Witchhunter"),
+            Self::GemlingLegionnaire => write!(f, "Gemling Legionnaire"),
+            Self::AcolyteOfChayula => write!(f, "Acolyte of Chayula"),
+            Self::Invoker => write!(f, "Invoker"),
+            Self::Deadeye => write!(f, "Deadeye"),
+            Self::Pathfinder => write!(f, "Pathfinder"),
+            Self::Chronomancer => write!(f, "Chronomancer"),
+            Self::Stormweaver => write!(f, "Stormweaver"),
+            Self::Titan => write!(f, "Titan"),
+            Self::Warbringer => write!(f, "Warbringer"),
+            Self::BloodMage => write!(f, "Blood Mage"),
+            Self::Infernalist => write!(f, "Infernalist"),
+        }
+    }
+}
+
+impl ClassAscendency {
+    pub fn get_class(&self) -> CharacterClass {
+        match self {
+            Self::Witchhunter | Self::GemlingLegionnaire => CharacterClass::Mercenary,
+            Self::AcolyteOfChayula | Self::Invoker => CharacterClass::Monk,
+            Self::Deadeye | Self::Pathfinder => CharacterClass::Ranger,
+            Self::Chronomancer | Self::Stormweaver => CharacterClass::Sorceress,
+            Self::Titan | Self::Warbringer => CharacterClass::Warrior,
+            Self::BloodMage | Self::Infernalist => CharacterClass::Witch,
+        }
+    }
+
+    pub fn get_discord_image_name(&self) -> &'static str {
+        match self {
+            Self::Witchhunter => "mercenary_witchhunter",
+            Self::GemlingLegionnaire => "mercenary_gemling_legionnaire",
+            Self::AcolyteOfChayula => "monk_acolyte_of_chayula",
+            Self::Invoker => "monk_invoker",
+            Self::Deadeye => "ranger_deadeye",
+            Self::Pathfinder => "ranger_pathfinder",
+            Self::Chronomancer => "sorceress_chronomancer",
+            Self::Stormweaver => "sorceress_stormweaver",
+            Self::Titan => "warrior_titan",
+            Self::Warbringer => "warrior_warbringer",
+            Self::BloodMage => "witch_blood_mage",
+            Self::Infernalist => "witch_infernalist",
+        }
+    }
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Translations {
+    pub areas: HashMap<String, String>,
+}
+
+impl Translations {
+    pub fn get_area_display_name(&self, area: &str) -> Option<&str> {
+        self.areas.get(area).map(|s| s.as_str())
+    }
+}
